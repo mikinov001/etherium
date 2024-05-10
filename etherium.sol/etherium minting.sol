b9b164ad4dc1d305e21747e31c4544b26313edba
Chetan Kochiyaniya <ckochiyaniya950@rku.ac.in>
2024-05-10 08:38:27 +0530

    Refactor MyNFT contract, improve security, and add comments

--- etherium.sol/etherium minting.sol
+++ etherium.sol/etherium minting.sol
@@ -5,23 +5,44 @@ import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 
 contract MyNFT is ERC721, Ownable {
-    uint256 public tokenCounter;
+    uint256 private _currentTokenId;
 
-    constructor() ERC721("MyNFT", "MNFT") {}
+    // Event to log each token minted
+    event TokenMinted(address indexed owner, uint256 indexed tokenId);
 
-    function mint(address recipient) public onlyOwner returns (uint256) {
-        uint256 tokenId = tokenCounter;
-        _safeMint(recipient, tokenId);
-        tokenCounter++;
-        return tokenId;
+    /**
+     * @dev Constructor function
+     */
+    constructor() ERC721("MyNFT", "MNFT") {
+        _currentTokenId = 0;
     }
 
-    function burn(uint256 tokenId) public onlyOwner {
+    /**
+     * @dev Function to mint new NFTs
+     * @param recipient The address to which the new NFT will be minted
+     * @return The token ID of the newly minted NFT
+     */
+    function mint(address recipient) external onlyOwner returns (uint256) {
+        _currentTokenId++;
+        _safeMint(recipient, _currentTokenId);
+        emit TokenMinted(recipient, _currentTokenId);
+        return _currentTokenId;
+    }
+
+    /**
+     * @dev Function to burn existing NFTs
+     * @param tokenId The ID of the NFT to burn
+     */
+    function burn(uint256 tokenId) external onlyOwner {
         require(_exists(tokenId), "Token does not exist");
         _burn(tokenId);
     }
 
-    function totalSupply() public view returns (uint256) {
-        return tokenCounter;
+    /**
+     * @dev Function to get the total number of tokens minted
+     * @return The total number of tokens minted
+     */
+    function totalSupply() external view returns (uint256) {
+        return _currentTokenId;
     }
 }
